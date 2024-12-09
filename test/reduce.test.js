d04a9a9ab48dd5f1988a45184cc5c3de0f6ef254
Affira <57002431+affira@users.noreply.github.com>
2024-12-10 00:50:43 +0200

    updated test for reduce for overall coverage

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -26,4 +26,17 @@ describe('reduce', () => {
     expect(reduce([], (sum, n) => sum + n, 0)).toBe(0)
     expect(reduce({}, (result, value) => value, 0)).toBe(0)
   })
+  test('baseReduce with initAccum and empty object', () => {
+    const object = {}
+    const result = reduce(object, (acc, value) => acc + value, 0)
+    expect(result).toBe(0)
+  })
+  
+  test('baseReduce with initAccum and non-empty object', () => {
+    const object = { 'a': 1, 'b': 2 }
+    const iteratee = jest.fn((result, num) => result + num)
+    const result = reduce(object, iteratee, 0)
+    expect(result).toBe(3)
+    expect(iteratee).toHaveBeenCalledTimes(2)
+  })
 })
