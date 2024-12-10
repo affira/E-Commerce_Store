c11baeddd6b2f3048372b3a5786290a9b90c3b99
Affira <57002431+affira@users.noreply.github.com>
2024-12-10 16:26:50 +0200

    attempt 2 for Coveralls setup

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,28 +1,27 @@
 export default {
   transform: {
-    '^.+\\.js$': ['babel-jest', { configFile: './babel.config.js' }],
+    '^.+\\.js$': 'babel-jest',
   },
   testEnvironment: 'node',
   moduleFileExtensions: ['js'],
   collectCoverageFrom: [
     'src/**/*.js',
-    '!src/.internal/**',
+    '!src/.internal/**'
   ],
-  coverageDirectory: './coverage', // specify a coverage directory
+  coverageReporters: ['text', 'lcov'],
   coverageThreshold: {
     global: {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80,
-    },
+      statements: 80
+    }
   },
   testMatch: [
-    '<rootDir>/test/**/*.test.js',
+    '<rootDir>/test/**/*.test.js'
   ],
   verbose: true,
   clearMocks: true,
   resetMocks: true,
-  restoreMocks: true,
-  cacheDirectory: '<rootDir>/.jest-cache',
+  restoreMocks: true
 };
