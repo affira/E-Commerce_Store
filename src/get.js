89e11d2f2ecd76de929bfeb2340710770ec7d2c4
Affira <57002431+affira@users.noreply.github.com>
2024-12-10 03:09:29 +0200

    Uploading correct files from the correct src

--- src/get.js
+++ src/get.js
@@ -1,31 +1,32 @@
+import baseGet from './.internal/baseGet.js'
+
 /**
- * Gets the value at path of object
+ * Gets the value at `path` of `object`. If the resolved value is
+ * `undefined`, the `defaultValue` is returned in its place.
  *
- * @param {Object} object The object to query
- * @param {Array|string} path The path of the property to get
- * @param {*} [defaultValue] The value returned for undefined resolved values
- * @returns {*} Returns the resolved value
+ * @since 3.7.0
+ * @category Object
+ * @param {Object} object The object to query.
+ * @param {Array|string} path The path of the property to get.
+ * @param {*} [defaultValue] The value returned for `undefined` resolved values.
+ * @returns {*} Returns the resolved value.
+ * @see has, hasIn, set, unset
+ * @example
+ *
+ * const object = { 'a': [{ 'b': { 'c': 3 } }] }
+ *
+ * get(object, 'a[0].b.c')
+ * // => 3
+ *
+ * get(object, ['a', '0', 'b', 'c'])
+ * // => 3
+ *
+ * get(object, 'a.b.c', 'default')
+ * // => 'default'
  */
 function get(object, path, defaultValue) {
-  if (object == null || path === undefined) {
-    return defaultValue;
-  }
-
-  if (path === '' || (Array.isArray(path) && path.length === 0)) {
-    return defaultValue;
-  }
-
-  const paths = Array.isArray(path) ? path : path.split(/[.\[\]]+/).filter(Boolean);
-  let result = object;
-
-  for (const p of paths) {
-    result = result?.[p];
-    if (result === undefined) {
-      return defaultValue;
-    }
-  }
-
-  return result;
+  const result = object == null ? undefined : baseGet(object, path)
+  return result === undefined ? defaultValue : result
 }
 
-export default get;
+export default get
